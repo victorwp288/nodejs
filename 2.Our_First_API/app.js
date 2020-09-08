2ed02372e8724a33f94304fe834a7d7f643ac299
vwp <victorwpetersen@gmail.com>
2020-09-08 11:25:04 +0200

    init

--- 2.Our_First_API/app.js
+++ 2.Our_First_API/app.js
@@ -1,1 +1,15 @@
-const express = require("express")
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) =>{
+    return res.send("hej med dig der du dig");
+});
+
+
+app.listen(8080, (error) => {
+    if (error) {
+        console.log("Error starting the server");
+    }
+    console.log("Server is running on port", 8080);
+});
+
